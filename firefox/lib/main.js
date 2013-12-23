40658cb59c0c155e9d4396b83a70ea87022808bc
cs <pkufashuo400@cs-pk.(none)>
2013-12-24 00:02:51 +0800

    first commit

---
+++ firefox/lib/main.js
@@ -0,0 +1,9 @@
+(function() {
+  var unblockYouku;
+
+  unblockYouku = {
+    defaultServer: 'www.yōukù.com/proxy.php',
+    backupServer: 'bak.yōukù.com/proxy.php'
+  };
+
+}).call(this);
---
+++ firefox/src/main.coffee
@@ -0,0 +1,6 @@
+
+unblockYouku = 
+  # only for redirect mode
+  defaultServer: 'www.yōukù.com/proxy.php'
+  backupServer: 'bak.yōukù.com/proxy.php'
+
